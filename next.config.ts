5771a13f4fcc7e81ea7c8a9b8104fabdf72de0a6
andyizyfleyr <andyizyfleur@gmail.com>
2026-04-11 14:09:58 +0100

    chore: update dependencies and remove Cloudflare deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
+import { getLocalIp } from "./lib/getLocalIp";
+
+const ip = getLocalIp();
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  allowedDevOrigins: [ip],
+
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
+      allowedOrigins: [
+        `http://localhost:3000`,
+        `http://${ip}:3000`,
+      ],
     },
   },
 };
